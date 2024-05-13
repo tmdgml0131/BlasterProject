c4700b52010ca08798d329c62af4e377ecc6808e
Seunghee Yun <tmdgml.yun@gmail.com>
2024-05-13 11:48:02 +0900

    Blaster - Version Switching
    Moved to 5.2 -> 5.4

--- Blaster/Plugins/MultiplayerSessions/Intermediate/Build/Win64/UnrealEditor/Inc/MultiplayerSessions/UHT/Menu.generated.h
+++ Blaster/Plugins/MultiplayerSessions/Intermediate/Build/Win64/UnrealEditor/Inc/MultiplayerSessions/UHT/Menu.generated.h
@@ -14,19 +14,7 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define MULTIPLAYERSESSIONS_Menu_generated_h
 
-#define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_SPARSE_DATA
-#define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_RPC_WRAPPERS \
- \
-	DECLARE_FUNCTION(execJoinButtonClicked); \
-	DECLARE_FUNCTION(execHostButtonClicked); \
-	DECLARE_FUNCTION(execOnStartSession); \
-	DECLARE_FUNCTION(execOnDestroySession); \
-	DECLARE_FUNCTION(execOnCreateSession); \
-	DECLARE_FUNCTION(execMenuSetup);
-
-
 #define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_RPC_WRAPPERS_NO_PURE_DECLS \
- \
 	DECLARE_FUNCTION(execJoinButtonClicked); \
 	DECLARE_FUNCTION(execHostButtonClicked); \
 	DECLARE_FUNCTION(execOnStartSession); \
@@ -35,7 +23,6 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 	DECLARE_FUNCTION(execMenuSetup);
 
 
-#define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_ACCESSORS
 #define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesUMenu(); \
@@ -45,36 +32,13 @@ public: \
 	DECLARE_SERIALIZER(UMenu)
 
 
-#define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_INCLASS \
-private: \
-	static void StaticRegisterNativesUMenu(); \
-	friend struct Z_Construct_UClass_UMenu_Statics; \
-public: \
-	DECLARE_CLASS(UMenu, UUserWidget, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/MultiplayerSessions"), NO_API) \
-	DECLARE_SERIALIZER(UMenu)
-
-
-#define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_STANDARD_CONSTRUCTORS \
-	/** Standard constructor, called after all reflected properties have been initialized */ \
-	NO_API UMenu(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
-	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UMenu) \
-	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UMenu); \
-	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UMenu); \
-private: \
-	/** Private move- and copy-constructors, should never be used */ \
-	NO_API UMenu(UMenu&&); \
-	NO_API UMenu(const UMenu&); \
-public: \
-	NO_API virtual ~UMenu();
-
-
 #define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_ENHANCED_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API UMenu(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
-	NO_API UMenu(UMenu&&); \
-	NO_API UMenu(const UMenu&); \
+	UMenu(UMenu&&); \
+	UMenu(const UMenu&); \
 public: \
 	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UMenu); \
 	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UMenu); \
@@ -83,24 +47,10 @@ public: \
 
 
 #define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_13_PROLOG
-#define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_GENERATED_BODY_LEGACY \
-PRAGMA_DISABLE_DEPRECATION_WARNINGS \
-public: \
-	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_SPARSE_DATA \
-	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_RPC_WRAPPERS \
-	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_ACCESSORS \
-	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_INCLASS \
-	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_STANDARD_CONSTRUCTORS \
-public: \
-PRAGMA_ENABLE_DEPRECATION_WARNINGS
-
-
 #define FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_SPARSE_DATA \
 	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_RPC_WRAPPERS_NO_PURE_DECLS \
-	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_ACCESSORS \
 	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_INCLASS_NO_PURE_DECLS \
 	FID_git_BlasterProject_Blaster_Plugins_MultiplayerSessions_Source_MultiplayerSessions_Public_Menu_h_16_ENHANCED_CONSTRUCTORS \
 private: \
--- Blaster/Plugins/MultiplayerSessions/Intermediate/Build/Win64/UnrealEditor/Inc/MultiplayerSessions/UHT/MultiplayerSessions.init.gen.cpp
+++ Blaster/Plugins/MultiplayerSessions/Intermediate/Build/Win64/UnrealEditor/Inc/MultiplayerSessions/UHT/MultiplayerSessions.init.gen.cpp
@@ -25,13 +25,13 @@ void EmptyLinkFunctionForGeneratedCodeMultiplayerSessions_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x7A32A28A,
-				0x3F867B98,
-				METADATA_PARAMS(nullptr, 0)
+				0x33C851A0,
+				0x23ABF583,
+				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_MultiplayerSessions.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_MultiplayerSessions.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MultiplayerSessions(Z_Construct_UPackage__Script_MultiplayerSessions, TEXT("/Script/MultiplayerSessions"), Z_Registration_Info_UPackage__Script_MultiplayerSessions, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x7A32A28A, 0x3F867B98));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MultiplayerSessions(Z_Construct_UPackage__Script_MultiplayerSessions, TEXT("/Script/MultiplayerSessions"), Z_Registration_Info_UPackage__Script_MultiplayerSessions, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x33C851A0, 0x23ABF583));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
